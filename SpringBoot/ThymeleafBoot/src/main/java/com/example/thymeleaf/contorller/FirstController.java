9bede135b0e569967227dfe205bbda4f580cf069
Shoyab <s.manjoor001@gmail.com>
2024-08-16 09:09:19 +0530

    added Iteration example of Thymeleaf

--- SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
+++ SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
@@ -1,9 +1,12 @@
 package com.example.thymeleaf.contorller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
@@ -18,6 +21,14 @@ public class FirstController {
 		model.addAttribute("name", "Demo Name");
 		model.addAttribute("city", "Modinagar");
 		return "about";
+	}
+
+	@RequestMapping(value = "/loop", method = RequestMethod.GET)
+	public String loopHandler(Model model) {
+
+		List<String> names = List.of("Abc", "Def", "Ghi", "Jkl");
+		model.addAttribute("names", names);
+		return "loop";
 	}
 
 }
