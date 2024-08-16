cbc43c299febde6ac18f7dc3a736c37271adfb43
Shoyab <s.manjoor001@gmail.com>
2024-08-16 10:17:58 +0530

    added Conditional example of Thymeleaf

--- SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
+++ SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
 
+	// controller for Home Page
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeHandler() {
 		return "home";
 	}
 
+	// Controller for About Page..
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String aboutMethod(Model model) {
 		model.addAttribute("name", "Demo Name");
@@ -23,12 +25,22 @@ public class FirstController {
 		return "about";
 	}
 
+	// Controller for Looping Page
 	@RequestMapping(value = "/loop", method = RequestMethod.GET)
 	public String loopHandler(Model model) {
-
 		List<String> names = List.of("Abc", "Def", "Ghi", "Jkl");
 		model.addAttribute("names", names);
 		return "loop";
+	}
+
+	// Controller for Conditional Statement Page
+	@RequestMapping(value = "/condition", method = RequestMethod.GET)
+	public String conditionHandler(Model m) {
+		m.addAttribute("isActive", "true");
+		m.addAttribute("gender", 'F');
+		List<Integer> list = List.of(1, 2, 3, 4, 5);
+		m.addAttribute("myList", list);
+		return "condition";
 	}
 
 }
