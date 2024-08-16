bfb9c5fd1aa9ce47091e5af979df07bffbc0221c
Shoyab <s.manjoor001@gmail.com>
2024-08-16 08:15:37 +0530

    added thymeleaf template example

--- SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
+++ SpringBoot/ThymeleafBoot/src/main/java/com/example/thymeleaf/contorller/FirstController.java
@@ -5,14 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class FirstController {
 
 	@RequestMapping(value = "/about", method = RequestMethod.GET)
 	public String aboutMethod(Model model) {
 		model.addAttribute("name", "Demo Name");
-		model.addAttribute("city", "Modinagar" );
+		model.addAttribute("city", "Modinagar");
 		return "about";
 	}
 
